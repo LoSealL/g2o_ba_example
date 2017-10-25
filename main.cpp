0e314587c4a4fe643c0d83d087aadc178e7ff585
Wenyi Tang <twytwy12345@live.com>
2017-10-25 17:36:33 +0800

    Add missing dll dependency

--- main.cpp
+++ main.cpp
@@ -31,9 +31,7 @@
 #include <g2o/core/robust_kernel_impl.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
 #include <g2o/solvers/csparse/linear_solver_csparse.h>
-#ifndef _WINDOWS
-  #include <g2o/solvers/cholmod/linear_solver_cholmod.h>
-#endif
+//#include <g2o/solvers/cholmod/linear_solver_cholmod.h>
 #include <g2o/types/slam3d/se3quat.h>
 #include <g2o/types/sba/types_six_dof_expmap.h>
 
